2a9439751f96d86164845c6ab34bba474e9f0b40
tdunn1248 <tdunn1248@gmail.com>
2017-08-22 14:33:32 -0700

    worked on toggle input

--- public/script.js
+++ public/script.js
@@ -1,7 +1,60 @@
-console.log("Sanity Check: JS is working!");
+const baseURL = 'https://mutably.herokuapp.com'
+const inputField = document.createElement('input')
+const saveBtn = document.createElement('button')
+saveBtn.classList.add('btn-success')
+saveBtn.classList.add('btn')
+saveBtn.classList.add('btn-sm')
+saveBtn.innerHTML = 'Save'
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  // code in here
+  getPokemon()
+  editButtonEventListener()
+  deleteButtonEventListener()
+})
 
-});
+function editButtonEventListener() {
+  $(document).on('click', '.btn-edit', function(event) {
+    console.dir(this.nextSibling.nextSibling);
+    toggleInputField(this.nextSibling.nextSibling)
+    toggleEditButton(this)
+  })
+}
+
+function deleteButtonEventListener() {
+  $(document).on('click', '.btn-delete', function() {
+    this.parentNode.remove()
+  })
+}
+
+function toggleInputField(editSelectedPokemon) {
+  inputField.classList.add('input-edit')
+  console.dir(inputField)
+  $(editSelectedPokemon).replaceWith(inputField)
+  $(inputField).attr('placeholder', editSelectedPokemon.innerHTML)
+}
+
+function toggleEditButton(btnSelected) {
+  $(btnSelected).replaceWith(saveBtn)
+}
+
+function getPokemon() {
+  $.ajax({
+    method: 'GET',
+    url: `${baseURL}/pokemon`,
+    success: renderPokemon
+  })
+}
+
+function renderPokemon(pokemonCollection) {
+  const pokemonArray = pokemonCollection.pokemon
+  pokemonArray.forEach(pokemon => {
+    $(".list-group").append(`
+      <div class='list-item'>
+        <button class='btn btn-info btn-sm btn-edit' value=${pokemon._id}>Edit</button>
+        <p class='pokemon-info'>${pokemon.name}, ${pokemon.pokedex}, ${pokemon.evolves_from}</p>
+        <button class='btn btn-danger btn-sm btn-delete' value=${pokemon._id}>Delete</button>
+      </div>
+    `)
+  })
+}
